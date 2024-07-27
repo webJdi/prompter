f5cbcc7c0a1a71b652d0d59fd02786a16cdcb0da
Suman Senapati <sigma.6.2009@gmail.com>
2024-07-28 00:54:15 +0530

    Created input.js component

---
+++ prompter/src/components/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Input({prompt, setPrompt})
+{
+    return(
+        <div>
+            <textarea
+            value={prompt}
+            onChange={(e)=> setPrompt(e.target.value)}
+            placeholder="Enter your prompt"
+            />
+        </div>
+    );
+}
+
+export default InputComponent;
