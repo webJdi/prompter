8e36c79adb5702a572d6a691580973fbfe3a0284
Suman Senapati <sigma.6.2009@gmail.com>
2024-07-29 00:50:26 +0530

    Corrected className

--- prompter/src/components/Output.js
+++ prompter/src/components/Output.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Output({ improvedPrompt }) {
   return (
-    <div class='result'>
+    <div className='result'>
       <h3>Suggested prompt:</h3>
       <div>
         <textarea type="text" value={improvedPrompt} id="resulttext">
